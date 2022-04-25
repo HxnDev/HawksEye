c8e695ae6818b8fb39becea54155b5b88a666919
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-25 13:48:09 +0500

    Updates

---
+++ Implementation/Mobile App/config/colors.js
@@ -0,0 +1,15 @@
+export default {
+
+    neonPurple: '#7a42f4',
+
+    //primary  :'#58588C',
+    //secondary:'#57A8A1',
+
+    primary  : 'white',
+    secondary:'#28D8A1',
+
+    DB      : '#F4DB7E',
+    SB      : '#9DA9F1',
+    ora     : '#FE6A3C',
+    Beie    : '#F4DB7E',
+}
