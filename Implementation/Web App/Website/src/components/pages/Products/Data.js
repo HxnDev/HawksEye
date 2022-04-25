acdfae40768b13e0d7fc7d4a7aac1b4caa4dde66
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-25 12:51:01 +0500

    Updates

---
+++ Implementation/Web App/Website/src/components/pages/Products/Data.js
@@ -0,0 +1,14 @@
+export const productObjOne = {
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: '',
+    headline: "HAWK'S ",
+    headline1: "EYE",
+    description:
+      'Get access to our exclusive product that will run in real-time and detect and report chosen abnormalities.',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: './images/hawks-eye.png',
+    alt: 'Credit Card'
+  };
