81d57cac7215085bee75fa1361c490c71860d370
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-25 12:34:40 +0500

    Updates

---
+++ Implementation/Web App/Flask/myutils.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+def clear_directory(path):
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
