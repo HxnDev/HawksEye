e4015682867bb218b80354cd971b3ef4cb590b9d
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-04-25 12:39:51 +0500

    Updates

---
+++ Implementation/Web App/Website/src/components/pages/Features/Aggressive.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Features from './Features';
+import { aggressive } from './AggressiveData';
+
+function Aggressive() {
+  return (
+    <>
+      <Features {...aggressive} />
+      
+    </>
+  );
+}
+
+export default Aggressive;
