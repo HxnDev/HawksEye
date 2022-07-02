58177d9972ba70a390c0cd0dd5a65e7204315d15
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2022-07-02 14:25:06 +0500

    Updated Index.js to latest React version & Updated Software Diagrams

--- Implementation/Web App/Website/src/index.js
+++ Implementation/Web App/Website/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import firebase from "firebase";
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
     <App />,
   document.getElementById('root')
 );
